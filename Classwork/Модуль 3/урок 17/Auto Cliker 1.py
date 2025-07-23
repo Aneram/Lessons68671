834df881335cc802c55f5ef1197ab0d5b3e9e273
Aneram <nariam778@gmail.com>
2025-07-23 18:10:15 +0300

    Homework

--- Classwork/Модуль 3/урок 17/Auto Cliker 1.py
+++ Classwork/Модуль 3/урок 17/Auto Cliker 1.py
@@ -1,14 +1,8 @@
-
-
-def exit_app():
-
-# тут буде завершення програми
-    root.destroy()
-
-
 import tkinter as tk
 from tkinter import messagebox
 import time
+import keyboard
+import mouse
 
 running = False # змінна, що зберігатиме стан: програма зараз працює або ні
 delay = 0 # змінна, що зберігатиме тривалість перерви після кожного кліку
@@ -24,10 +18,10 @@ def start_clicker():
     schedule_click()
 
 def schedule_click():
-    if running:
-        print("Клац!")  # тут потім додамо клацання миші замість print
-    time.sleep(delay)  # затримка між кліками
-    schedule_click()  # функція знову викликає сама себе
+        while running:
+            mouse.click()  # тут потім додамо клацання миші замість print
+            time.sleep(delay)  # затримка між кліками
+            # функція знову викликає сама себе
 
 def exit_app():
 
@@ -42,15 +36,22 @@ def exit_app():
 def show_info(event):
     messagebox.showinfo("Інформація", "Це автоклікер, він буде клікати мишкою зі швидкістю, яку ти вкажеш!")
 
+def exit_app():
+    print("STOP")
+    global running
+    running = False
+    messagebox.showinfo("Auto Clicker", "Auto Clicker зупинено.")
+    root.destroy()
+
 root = tk.Tk()
 root.bind('i', show_info)
 
 root.title("Привітання")
-root.geometry("450x200")
+root.geometry("570x200")
 root.configure(bg="pink")
 
 label = tk.Label(root, text="Автоклікер! (для ледачих хто не хоче клікати)",
-                 font=("Arial", 16),fg="#9339E1", bg="pink")
+                 font=("Segoe Script", 16),fg="#9339E1", bg="pink")
 
 label.pack(pady=10)
 
@@ -60,10 +61,19 @@ entry.pack(pady=10)
 # greeting_label = tk.Label(root, text="", font=("Arial", 16))
 # greeting_label.pack(pady=20)
 
-exit_button = tk.Button( text="Вийти", font=("Arial", 14), bg="red", fg="black", width=10)
+exit_button = tk.Button( text="Вийти", font=("SimSun", 14), bg="red", fg="black", width=10)
 exit_button.pack(side="left", padx=30)
 
-start_button = tk.Button( text="Почати", font=("Arial", 14), bg="lime green", fg="black", width=10, command=start_clicker)
+def exit_app():
+
+    global running
+    running = False
+    messagebox.showinfo("Auto Clicker", "Auto Clicker зупинено.")
+    root.destroy() # Закриття вікна Tkinter
+
+keyboard.add_hotkey('esc', exit_app)
+
+start_button = tk.Button( text="Почати", font=("Mongolian Baiti", 14), bg="lime green", fg="black", width=10, command=start_clicker)
 start_button.pack(side="right", padx=30)
 
 root.mainloop()
